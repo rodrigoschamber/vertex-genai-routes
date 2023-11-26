de15c92bd29f6a1a040c1e465e0799d8985fb733
Rodrigo Schamber <rodrigoschamber@gmail.com>
2023-11-26 09:39:25 -0300

    First version of http server

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const {
+import express from "express";
+import {
   sendRequest,
   writeResponseLocally,
-} = require("./utils/google-foundation-models");
+} from "./utils/google-foundation-models.js";
+
+const app = express();
 
 const city = `Ouro Preto-MG`;
 const duration = `3`;
@@ -10,18 +13,24 @@ const promptModel = () => {
   return `Sou um viajante sem experiência e preciso gerenciar roteiros de viagens contendo os principais pontos de interesse culturais e gastronômicos. Para isto preciso de um roteiro detalhado contendo hotéis, pousadas, museus e restaurantes. Sou amante da natureza, por isso é preciso incluir parques naturais, cachoeiras e mirantes. Meu destino é ${city} e gostaria de um roteiro de ${duration} dias.`;
 };
 
-const params = {
-  apiEndpoint: "us-central1-aiplatform.googleapis.com",
-  projectId: "ai-2023-405518",
-  modelId: "text-bison@001",
-  instances: [{ content: promptModel() }],
-  parameters: { temperature: 0.2, maxOutputTokens: 256, topP: 0.8, topK: 40 },
-};
-sendRequest(params)
-  .then((response) => {
-    console.log("Follow your route: \n", response.predictions[0].content);
-    writeResponseLocally(params, response.predictions[0]);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+app.get("/tour/city", async (_, res) => {
+  const params = {
+    apiEndpoint: "us-central1-aiplatform.googleapis.com",
+    projectId: "ai-2023-405518",
+    modelId: "text-bison@001",
+    instances: [{ content: promptModel() }],
+    parameters: { temperature: 0.2, maxOutputTokens: 256, topP: 0.8, topK: 40 },
+  };
+  sendRequest(params)
+    .then((response) => {
+      res.json(response.predictions[0].content);
+      writeResponseLocally(params, response.predictions[0]);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`🚀 Server listening on port ${PORT}`));
--- utils/google-foundation-models.js
+++ utils/google-foundation-models.js
@@ -1,33 +1,35 @@
-const { GoogleAuth } = require('google-auth-library');
-const fs = require('fs');
-const path = require('path');
-async function sendRequest(options) {
+import { GoogleAuth } from "google-auth-library";
+import fs from "fs";
+import path from "path";
+
+export async function sendRequest(options) {
   const auth = new GoogleAuth({
     scopes: "https://www.googleapis.com/auth/cloud-platform",
   });
   const client = await auth.getClient();
   const accessToken = (await client.getAccessToken()).token;
   const data = { instances: options.instances, parameters: options.parameters };
-  
+
   const response = await fetch(
-    `https://${options.apiEndpoint}/v1/projects/${options.projectId}/locations/us-central1/publishers/google/models/${options.modelId}:predict`, 
+    `https://${options.apiEndpoint}/v1/projects/${options.projectId}/locations/us-central1/publishers/google/models/${options.modelId}:predict`,
     {
-      method: 'POST',
+      method: "POST",
       headers: {
         Authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     }
   );
-  
+
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
-  
+
   return await response.json();
 }
-async function writeResponseLocally(params, response) {
+
+export async function writeResponseLocally(params, response) {
   const outputDir = path.join(__dirname, "../routes_output");
   fs.writeFile(
     path.join(outputDir, `${params.modelId}.json`),
@@ -38,23 +40,3 @@ async function writeResponseLocally(params, response) {
     }
   );
 }
-async function saveCodeToFile(params, response) {
-  const fileContent = response.predictions[0].content;
-  const fileTypePattern = /^```(\w+)\n/;
-  const fileTypeMatch = fileContent.match(fileTypePattern);
-  const fileType = fileTypeMatch ? fileTypeMatch[1] : "txt"; // default to .txt if no match found
-  const outputDir = path.join(__dirname, "../examples_output");
-
-  const cleanedCode = fileContent
-    .replace(fileTypePattern, "")
-    .replace(/```$/, "");
-  fs.writeFile(
-    path.join(outputDir, `${params.modelId}.${fileType}`),
-    cleanedCode,
-    function (err) {
-      if (err) return console.log(err);
-      console.log(`Writing to ${params.modelId}.${fileType}`);
-    }
-  );
-}
-module.exports = { sendRequest, writeResponseLocally, saveCodeToFile };
