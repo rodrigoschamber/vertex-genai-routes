c40c84c6157b189b5ef3e39024ac051fb89d4454
Rodrigo Schamber <rodrigoschamber@gmail.com>
2023-11-25 21:37:03 -0300

    Added first prompt model

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const {
   sendRequest,
   writeResponseLocally,
 } = require("./utils/google-foundation-models");
+
+const promptModel = () => {
+  return `Sou um viajante sem experiência e preciso gerenciar roteiros de viagens contendo os principais pontos de interesse culturais e gastronômicos. Para isto preciso de um roteiro detalhado contendo hotéis, pousadas, museus e restaurantes. Sou amante da natureza, por isso é preciso incluir parques naturais, cachoeiras e mirantes. Meu destino é Ouro Preto - MG`;
+};
+
 const params = {
   apiEndpoint: "us-central1-aiplatform.googleapis.com",
   projectId: "ai-2023-405518",
-  modelId: "text-bison",
-  instances: [
-    { content: "crie um roteiro turistico de tres dias para ouro preto mg" },
-  ],
+  modelId: "text-bison@001",
+  instances: [{ content: promptModel() }],
   parameters: { temperature: 0.2, maxOutputTokens: 256, topP: 0.8, topK: 40 },
 };
 sendRequest(params)
