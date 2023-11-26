fc0ae0c25b35ed4f10962ecafe948e7b72de4517
Rodrigo Schamber <rodrigoschamber@gmail.com>
2023-11-26 15:46:32 -0300

    added style to the page

--- index.js
+++ index.js
@@ -30,6 +30,22 @@ app.get("/tour/:lang/:city/:duration", async (req, res) => {
             <head>
               <meta charset="utf-8">
               <title>AI HACKATON 2023</title>
+              <style>
+                body {
+                    background-image: url('https://i.ibb.co/FgK3qmy/background.jpg');
+                    background-size: cover;
+                    background-position: center;
+                    background-attachment: fixed;
+                    margin: 0;
+                    padding: 0;
+                    height: 100vh;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                    color: white;
+                }
+              </style>
             </head>
             <body>
               <h1>AI HACKATON 2023</h1>
